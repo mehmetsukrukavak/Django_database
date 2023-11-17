abe7d16f1c46a7c88c61dc27feb0962c11a291ed
Mehmet Sukru Kavak <mehmetsukru.kavak07@gmail.com>
2023-11-17 07:59:30 +0300

    model create, insert data

--- django_models_databases/database_app/models.py
+++ django_models_databases/database_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Musician(models.Model):
+    name = models.CharField(max_length=50)
+    instrument = models.CharField(max_length=30)
+    age = models.IntegerField()
+
+    
